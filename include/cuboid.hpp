f7a41fe5dc214bcf7786c51ea6dcd98137094133
Krzysztof Gliwinski <259282@student.pwr.edu.pl>
2021-05-07 20:55:09 +0200

    Began Cuboid class

--- include/cuboid.hpp
+++ include/cuboid.hpp
@@ -1,8 +1,21 @@
 #include"vector3D.hpp"
 #include"matrix3D.hpp"
 
-class cuboid{
+class Cuboid{
+private:
 
+Vector3D tops[2][4];
 
+public:
+
+Cuboid();
+
+Cuboid(Vector3D const (&tab)[2][4]);
+
+void get_cub(Vector3D (&tab)[2][4]);
+
+Cuboid translation(Vector3D const &tran) const;
+
+Cuboid translation() const;
 
 };
--- src/cuboid.cpp
+++ src/cuboid.cpp
@@ -1,1 +1,13 @@
 #include"cuboid.hpp"
+
+
+/*******************************************************************
+ *  Konstruktor klasy Cuboid.                                              
+ *  Argumenty:                                                                
+ *      Brak argumentow.                                                      
+ *  Zwraca:                                                                   
+ *     \post Zwraca osiem wiercholkow                                   
+ */
+Cuboid::Cuboid(){
+
+}
